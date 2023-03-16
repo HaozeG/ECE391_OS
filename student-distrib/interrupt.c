35e5ee102b949a8c1740e5ac4cb7de2eb107486d
Haoze <haozeg2@illinois.edu>
2023-03-16 14:42:32 -0500

    magic number; modify printing style

--- student-distrib/interrupt.c
+++ student-distrib/interrupt.c
@@ -4,6 +4,8 @@
 #define SCAN_SIZE 255
 #define PRESS_MAX 0x57
 
+#define KEYBOARD_IRQ 1
+
 /*
  * keyboard_handler
  *   DESCRIPTION: Handle keyboard interrupts. When number or letter key is
@@ -21,14 +23,14 @@ void keyboard_handler()
          'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\0', '\0',
          'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', '\0', '\\',
          'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\0', '*', '\0', ' ', '\0'};
-    send_eoi(1);
+    send_eoi(KEYBOARD_IRQ);
     scan_code = inb(0x60) & 0xff; // read scan code
     if (scan_code > PRESS_MAX)    // only display when key is pressed
         return;
 
     ascii = scancode_table[scan_code];
-    if (ascii >= 0x61 && ascii <= 0x7A || ascii >= 0x30 && ascii <= 0x39) // if the key pressed is number or letter, print
+    if ((ascii >= 0x61 && ascii <= 0x7A) || (ascii >= 0x30 && ascii <= 0x39)) // if the key pressed is number or letter, print
     {
-        printf("%c\n", ascii);
+        printf("%c", ascii);
     }
 }
