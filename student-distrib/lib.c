8e0bc45f1164f5d8e03fe27bd60dd3e0820ec0bc
Haoze <haozeg2@illinois.edu>
2023-03-18 19:07:58 -0500

    wrap x and auto newline

--- student-distrib/lib.c
+++ student-distrib/lib.c
@@ -178,8 +178,9 @@ void putc(uint8_t c) {
         *(uint8_t *)(video_mem + ((NUM_COLS * screen_y + screen_x) << 1)) = c;
         *(uint8_t *)(video_mem + ((NUM_COLS * screen_y + screen_x) << 1) + 1) = ATTRIB;
         screen_x++;
-        screen_x %= NUM_COLS;
+        // Auto wrap(increment y before resetting x)
         screen_y = (screen_y + (screen_x / NUM_COLS)) % NUM_ROWS;
+        screen_x %= NUM_COLS;
     }
 }
 
