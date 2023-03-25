bcf03a2fb6034257943e7c3af5a65963bee75ae1
Haoze <haozeg2@illinois.edu>
2023-03-25 15:50:58 -0500

    finish keyboard and terminal

--- student-distrib/interrupt.c
+++ student-distrib/interrupt.c
@@ -11,6 +11,7 @@ int count_char = 0; // valid byte numbers in kbd buffer
 int enter_buf = 0;
 unsigned char kbd_buffer[] = {'\0'};
 
+#define MAX_BUF 128
 /*
  * keyboard_handler
  *   DESCRIPTION: Handle keyboard interrupts, display pressed key to screen
@@ -35,9 +36,10 @@ void keyboard_handler()
              '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\0', '*', '\0', ' ', '\0'},
             // 01
             {'\0', '\0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\0',
-             '\0', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', '\0',
-             '\0', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', '~', '\0',
-             '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', '\0', '*', '\0', ' ', '\0'},
+             '\0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\0',
+             '\0', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~','\0', 
+             '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '\0', '*', '\0', ' ', '\0'},
+
             // 10
             {'\0', '\0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '\0',
              '\0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', '\0',
@@ -45,9 +47,9 @@ void keyboard_handler()
              '\\', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', ',', '.', '/', '\0', '*', '\0', ' ', '\0'},
             // 11
             {'\0', '\0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '\0',
-             '\0', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\0',
-             '\0', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',
-             '\0', '|', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '\0', '*', '\0', ' ', '\0'},
+             '\0', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '{', '}', '\0',
+             '\0', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ':', '"', '~', '\0',
+             '|', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<', '>', '?', '\0', '*', '\0', ' ', '\0'}
         };
 
     send_eoi(KEYBOARD_IRQ);
@@ -147,6 +149,100 @@ void keyboard_handler()
  */
 void handle_backspace()
 {
-
+    if (count_char == 0) {
+        return;
+    }
+    delc();
+    count_char--;
     return;
+}
+
+// --- Terminal ---
+
+int32_t terminal_open() {
+    clear();
+    // Clears buf
+    count_char = 0;
+    // int32_t i;
+    // for (i = 0; i < MAX_BUF; i++) {
+    //     terminal_buf[i] = 0;
+    // }
+
+    return 0;
+}
+
+int32_t terminal_close(int32_t fd) {
+    return 0;
+}
+
+/*
+* terminal_read
+*   DESCRIPTION: Read from the keyboard buffer into buf. Return if enter is pressed
+*   INPUTS: fd - file descriptor
+*           buf - the buffer that is copied into
+*           n_bytes - maximum bytes to copy
+*   RETURN VALUE: number of bytes copied(include \n) on success,
+*                 -1 on failure
+*   SIDE EFFECTS: Only returns until enter is pressed; last char in buf is '\n'
+*/
+int32_t terminal_read(int32_t fd, void* buf, int32_t n_bytes) {
+    int32_t i;
+    char* buf_to = (char *)buf;
+    if (!buf_to || n_bytes > MAX_BUF) {
+        return -1;
+    }
+    cli();
+    // Wait until enter pressed
+    while (!enter_buf) {
+        sti();
+        for (i = 0; i < 1000; i++) {}
+        cli();
+    };
+
+    i = 0;
+    cli();
+    // MAX_BUF - 1: up to 127 characters
+    while (i < (MAX_BUF - 1) && kbd_buffer[i] != '\n' && i < n_bytes)
+    {
+        buf_to[i] = kbd_buffer[i];
+        kbd_buffer[i] = '\0';
+        i++;
+    }
+    buf_to[i] = '\n';
+    i++;
+
+    // reset enter indicator
+    enter_buf = 0;
+    count_char = 0;
+    sti();
+    return i;
+}
+
+/*
+* terminal_write
+*   DESCRIPTION: Write from buf to screen. 
+*   INPUTS: fd - file descriptor
+*           buf - the buffer that is write from
+*           n_bytes - number of bytes to write
+*   RETURN VALUE: number of bytes written(does not include '\n') on success, -1 on failure
+*   SIDE EFFECTS: Change content on screen
+*/
+int32_t terminal_write(int32_t fd, const void* buf, int32_t n_bytes) {
+    int32_t i;
+    char* buf_from = (char *)buf;
+    if (!buf_from || n_bytes > MAX_BUF) {
+        return -1;
+    }
+
+    i = 0;
+    cli();
+    while (i < n_bytes && buf_from[i] != '\n') {
+        putc(buf_from[i]);
+        i++;
+    }
+    // putc('\n');
+    // i++;
+
+    sti();
+    return i;
 }
--- student-distrib/interrupt.h
+++ student-distrib/interrupt.h
@@ -38,4 +38,14 @@ extern void kbd_init();
 extern void keyboard_handler();
 extern void handle_backspace();
 
+
+// Initialize terminal
+extern int32_t terminal_open();
+// Clears terminal specific variables
+extern int32_t terminal_close(int32_t fd);
+// Read n_bytes char from kbd_buf into buf, return the number of bytes read
+extern int32_t terminal_read(int32_t fd, void* buf, int32_t n_bytes);
+// Write n_bytes char from buf to screen
+extern int32_t terminal_write(int32_t fd, const void* buf, int32_t n_bytes);
+
 #endif
