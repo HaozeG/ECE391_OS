8a318c8240194264a667c50e7506870e3ebe5923
Haoze <haozeg2@illinois.edu>
2023-05-04 10:01:57 -0500

    test

--- syscalls/ece391touch.c
+++ syscalls/ece391touch.c
@@ -6,6 +6,7 @@
 int main() {
     int32_t fd, cnt;
     uint8_t buf[1024];
+    uint8_t buf_[64001];
 
     if (0 != ece391_getargs (buf, 1024)) { // get the filename
         ece391_fdputs (1, (uint8_t*)"could not read arguments\n");
@@ -22,14 +23,25 @@ int main() {
         return 3;
     }
     ece391_close(fd);
+    // TODO: Here I have a loop to assign value for buf
+    // And manually change cnt
+    int i;
+    for (i = 0; i < 64000; i++) {
+        buf_[i] = '2';
+    }
+    buf_[i] = 0;
+    cnt = 64000;
+
     if (-1 == (fd = ece391_open ((uint8_t*)buf))) {
         ece391_fdputs (1, (uint8_t*)"file open failed\n");
         return 2;
     }
-    cnt = ece391_strlen(buf); // get the length of the filename
-    // buf[cnt] = '\0';
-    // cnt++;
-    if (-1 == ece391_write (fd, buf, cnt)){
+    cnt = ece391_strlen(buf_); // get the length of the filename
+    cnt = 10000;
+    buf[cnt] = '\0';
+    cnt++;
+
+    if (-1 == ece391_write (fd, buf_, cnt)){
         return 3;
     }
 
